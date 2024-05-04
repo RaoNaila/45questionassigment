68a50f29ea98b28911e9d2d3e732456cfdbb6988
RaoNaila <160322743+RaoNaila@users.noreply.github.com>
2024-05-03 20:46:32 -0700

    question18

---
+++ coding/project18/main.ts
@@ -0,0 +1,44 @@
+//itquestion 18 from 45 assigment
+// place 5 plasec which you like to visit
+
+let places :string[]=["Saudia arabia","Turkey","Egypt","Palastine","Russia"];
+
+//prin it its original  in state
+
+console.log("original state:", places);
+
+//now print above array in alphabetic order
+
+console.log("Alphabetical order:", places.slice().sort());
+
+//again print it in its original order to show your array is still in its original order
+console.log("original state:", places);
+
+//now print in reverse alphabetic order
+console.log("Reverse Alphabetic ordr:",places.slice().sort().reverse());
+
+//now show again your array in its original order.
+console.log("original state after reverse:", places);
+
+//print your original array  in revese order 
+  places.reverse();
+  console.log("Reverse order of original array:", places);
+
+  //agian print your array in its original order
+  places.reverse();
+  console.log(" again Reverse order to original array:", places);
+
+  // sort your array so its stored in alphbetic order.
+  console.log(" Sort in alphabetical order:", places.slice().sort());
+
+  //sort to changr your arrays so its stored in reverse alphsbetic order order.
+
+  console.log("Stored in reverse Alphabetic ordr:",places.slice().sort().reverse());
+
+
+
+//==================completed==============//
+  
+
+
+
