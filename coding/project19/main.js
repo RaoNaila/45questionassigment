3e0be5d2a479fb66c56693f53eb2128267838aba
RaoNaila <160322743+RaoNaila@users.noreply.github.com>
2024-05-03 21:06:08 -0700

    question 19

---
+++ coding/project19/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// question 19 from array question 14
+let guestList = ["Naila", "Shabana", "Farhana"];
+// print a message indicating the numberof people you are inviting to dinner.
+//usinf for each method
+let lengthtList = guestList.length;
+console.log(`we are inviting total ${lengthtList} guest`);
---
+++ coding/project19/main.ts
@@ -0,0 +1,12 @@
+// question 19 from array question 14
+let guestList:string[]=["Naila","Shabana","Farhana"];
+
+// print a message indicating the numberof people you are inviting to dinner.
+//usinf for each method
+
+let lengthtList:number=guestList.length;
+
+
+
+console.log(`we are inviting total ${lengthtList} guest`);
+
